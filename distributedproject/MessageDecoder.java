2481d685b7bd70c01d4c38367f3874e27d6c5395
Nipuna-Sankalpa <nipunasankalpa.12@cse.mrt.ac.lk>
2017-01-09 23:16:11 +0530

    join message value issue fixed

--- distributedproject/MessageDecoder.java
+++ distributedproject/MessageDecoder.java
@@ -34,7 +34,7 @@ public class MessageDecoder extends RequestHandler {
 
         if (msg.contains("REGOK")) {
             registerResponse(msg);
-        } else if (msg.contains("UNREGOK")) {
+        } else if (msg.contains("UNROK")) {
             unregisterResponse(msg);
         } else if (msg.contains("JOINOK")) {
             joinResponse(msg);
@@ -65,6 +65,7 @@ public class MessageDecoder extends RequestHandler {
 
     private void unregisterResponse(String message) throws Exception {
         String buffer[] = message.split(" ");
+        System.out.println(message);
         if (buffer[2].equals("9999")) {
             /*
                 solution has to be a group decision
